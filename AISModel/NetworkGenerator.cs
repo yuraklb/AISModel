5ea84479892157a113c845a22b7a0dc061d33217
yura_k <yura_k@pc-service.dp.ua>
2016-05-02 17:28:50 +0300

    add NetworkGenerator.cs

---
+++ AISModel/NetworkGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AISModel
+{
+    internal class NetworkGenerator
+    {
+
+        public static List<Device> GenerateDevices(int mCountDevices, int mTmpPerc = 5)
+        {
+            List<Device> mDevices;
+
+            Random rnd = new Random();
+
+            //Generate network devices
+            for(int i = 0; i < mCountDevices; i++) {
+                mDevices.Add(new Device(i));
+            }
+
+            for(int i = 0; i < mCountDevices; i++) {
+                GenerateConnectedLinks(i);
+            }
+
+        }
+    }
+}
